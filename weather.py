297bd98430af8000375d93612b405f819496bbbc
Sulimanalmoneef8 <almoneef.s@gmail.com>
2026-06-08 15:34:33 +0300

    initial commit

---
+++ weather.py
@@ -0,0 +1,8 @@
+print("Program to check if a jacket is suitable")
+
+weather = int(input("\n Please enter the number of degrees celcius: "))
+
+if weather >= 25:
+    print("It is not suitable to wear a jacket")
+else:
+    print("It is suitable to wear a jacket")
